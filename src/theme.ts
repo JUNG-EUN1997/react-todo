2f765340e6f14220d5ab15c6222cf852fe40eb1f
JUNG-EUN1997 <rro0628@gmail.com>
2022-02-07 16:00:46 +0900

    todo list setup

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#353b48",
+  textColor: "#f5f6fa",
+  accentColor: "#4cd137",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#f5f6fa",
+  textColor: "#353b48",
+  accentColor: "#4cd137",
+};
